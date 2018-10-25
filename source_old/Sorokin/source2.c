4c8b87274036a666def316f21fbaa1ed2970b811
Дмитрий <44235516+DmitrySorok98@users.noreply.github.com>
2018-10-25 13:52:28 +0300

    Update source2.c

--- source_old/Sorokin/source2.c
+++ source_old/Sorokin/source2.c
@@ -3,6 +3,7 @@
 #define MAX_FILE_SIZE 1000
 /*Pozvolyaet opredelyat imya novogo tipa dannyh BYTE*/
 typedef unsigned char BYTE;
+/*perechislenie chvetov*/
 enum colors {blue, green, red};
 
 /*Pozvolyaet opredelyat imya novogo tipa dannyh PIXEL*/
@@ -17,12 +18,14 @@ main()
     int w;
     int h;
     int end[7];
+/*Otkrivaet 1 fail and schitivaet*/
     FILE * fin = fopen("1.bmp", "r");
     perror("Open file");
-
+/*Otkrivaet 2 fail and zapisivaet*/
     FILE * fout = fopen("2.bmp", "w");
     perror("Open file");
 
+/*schitivaet*/
     fread(&type, 2, 1, fin);
     fread(&size, 4, 1, fin);
     fread(&res, 4, 1, fin);
@@ -32,7 +35,8 @@ main()
     fread(&h, 4, 1, fin);
     fread(end, 4, 7, fin);
     PIXEL rastr[MAX_FILE_SIZE][MAX_FILE_SIZE];
-    
+
+/*schitivaet pixeli*/
 int padding=(size-off-h*w*3)/h;
 BYTE pad[20];   
 int i,j;
@@ -42,6 +46,7 @@ for(i=0; i<h ; i++)
 		fread(&rastr[i][j], 3, 1, fin);
         fread(pad, padding, 1, fin);        
 } 
+/*new visota and zapis faila*/
     int new_w = w*3;
     fwrite(&type, 2, 1, fout);
     fwrite(&size, 4, 1, fout);
@@ -51,6 +56,7 @@ for(i=0; i<h ; i++)
     fwrite(&new_w, 4, 1, fout);
     fwrite(&h, 4, 1, fout);
     fwrite(end, 4, 7, fout);
+/*rabochii kod (rastagivaet izobrah v 3 raza)*/
 for(i=0; i<h ; i++)
 {
 	for(j=0; j<w ; j++)
